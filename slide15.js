c19074ba7c34be2d3b9c3090cb0f1ce7d431961f
famm19 <fammii1901@gmail.com>
2024-07-18 14:28:18 +0700

    Add files via upload

---
+++ slide15.js
@@ -0,0 +1,38 @@
+(function(){var loadHandler=window['sl_{B23D0DA2-68E8-46B2-9671-F88E398586BA}'];loadHandler&&loadHandler(14, '<div id="spr0_4761a5f4"><div id="spr1_4761a5f4" class="kern slide"><img id="img6_4761a5f4" src="data/img2.png" width="213px" height="378px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_4761a5f4" class="kern slide"><div id="svg0_4761a5f4" style="left:-7.771px;top:333.716px;"><svg width="228" height="48" viewBox="-6.493 -6.493 228 48"><path fill="#4f81bd" stroke="#1c334e" stroke-width="2.002" stroke-linecap="butt" stroke-linejoin="round" d="M1.001,6.493 a5.492,5.492 0 0,1 5.492,-5.492 h201.946 a5.492,5.492 0 0,1 5.492,5.492 v21.967 a5.492,5.492 0 0,1 -5.492,5.492 h-201.946 a5.492,5.492 0 0,1 -5.492,-5.492 Z" fill-rule="evenodd"/></svg></div><div id="svg1_4761a5f4" style="left:-77.905px;top:-240.045px;"><svg width="335" height="231" viewBox="-1.001 -1.001 335 231"><ellipse fill="#195185" stroke="#1c334e" stroke-width="2.002" stroke-linecap="butt" stroke-linejoin="round" cx="166.366" cy="114.238" rx="165.365" ry="113.237"/></svg></div><div id="spr3_4761a5f4" style="left:7.736px;top:9.113px;"><div style="width:0px;"><span id="txt0_4761a5f4" data-width="66.680374" style="left:2.839px;top:0.449px;">EXAMPLE</span></div></div><div id="svg2_4761a5f4" style="left:93.137px;top:344.646px;"><svg width="30" height="24" viewBox="-1.001 -1.001 30 24"><ellipse fill="#4f81bd" stroke="#1c334e" stroke-width="2.002" stroke-linecap="butt" stroke-linejoin="round" cx="13.57" cy="10.788" rx="12.569" ry="9.787"/></svg></div><div id="spr4_4761a5f4" aria-label="Home with solid fill" role="group" style="left:97.52px;top:344.692px;"><div id="spr5_4761a5f4"><a id="hl0_4761a5f4" href="#" onclick="document.getElementById(\'coreSpr_6210335\').getCore().processTriggerEffect(this);document.getElementById(\'coreSpr_6210335\').getCore().gotoSlide(2, this);return false;"><img id="img0_4761a5f4" src="data/img14.svg#svgView(preserveAspectRatio(none);viewBox(0 0 96 96))" width="21.454" height="22.021" alt=""/></a></div></div><div id="spr6_4761a5f4" style="left:97.995px;top:364.978px;"><div style="width:0px;"><span id="txt1_4761a5f4" class="nokern" data-width="14.487373" style="left:2.839px;top:0.719px;">HOME</span></div></div><div id="svg3_4761a5f4" style="left:14.415px;top:344.217px;"><svg width="30" height="25" viewBox="-1.001 -1.001 30 25"><ellipse fill="#4f81bd" stroke="#1c334e" stroke-width="2.002" stroke-linecap="butt" stroke-linejoin="round" cx="13.57" cy="11.099" rx="12.569" ry="10.098"/></svg></div><div id="spr7_4761a5f4" aria-label="Back with solid fill" role="group" style="left:19.179px;top:344.544px;"><div id="spr8_4761a5f4"><a id="hl1_4761a5f4" href="#" onclick="document.getElementById(\'coreSpr_6210335\').getCore().processTriggerEffect(this);document.getElementById(\'coreSpr_6210335\').getCore().gotoPreviousSlide(this);return false;"><img id="img1_4761a5f4" src="data/img9.svg#svgView(preserveAspectRatio(none);viewBox(0 0 96 96))" width="21.301" height="21.88" alt=""/></a></div></div><div id="spr9_4761a5f4" style="left:19.229px;top:365.03px;"><div style="width:0px;"><span id="txt2_4761a5f4" class="nokern" data-width="11.989829" style="left:2.839px;top:0.719px;">BACK</span></div></div><div id="svg5_4761a5f4" style="left:159.107px;top:345.607px;"><svg width="49" height="27" viewBox="-3.799 -3.799 49 27"><a xlink:href="#" onclick="document.getElementById(\'coreSpr_6210335\').getCore().processTriggerEffect(this);document.getElementById(\'coreSpr_6210335\').getCore().gotoNextSlide(this);return false;" fill="none"><path fill="#4f81bd" stroke="#1c334e" stroke-width="2.002" stroke-linecap="butt" stroke-linejoin="round" d="M1.001,3.799 a2.798,2.798 0 0,1 2.798,-2.798 h33.628 a2.798,2.798 0 0,1 2.798,2.798 v11.191 a2.798,2.798 0 0,1 -2.798,2.798 h-33.628 a2.798,2.798 0 0,1 -2.798,-2.798 Z" fill-rule="evenodd"/></a></svg></div><div id="spr10_4761a5f4" style="left:162.906px;top:349.405px;"><div style="width:0px;"><a id="hl2_4761a5f4" href="#" onclick="document.getElementById(\'coreSpr_6210335\').getCore().processTriggerEffect(this);document.getElementById(\'coreSpr_6210335\').getCore().gotoNextSlide(this);return false;"><span id="txt3_4761a5f4" class="nokern" data-width="24.928627" style="left:8.154px;top:2.242px;">LANJUT</span></a></div></div><div id="svg6_4761a5f4" style="left:-12.743px;top:19.698px;"><svg width="240" height="343" viewBox="-20.081 -20.081 240 343"><path fill="#ffffff" fill-opacity="0.95" stroke="#1c334e" stroke-width="2.002" stroke-linecap="butt" stroke-linejoin="round" d="M1.001,20.081 a19.08,19.08 0 0,1 19.08,-19.08 h158.743 a19.08,19.08 0 0,1 19.08,19.08 v261.859 a19.08,19.08 0 0,1 -19.08,19.08 h-158.743 a19.08,19.08 0 0,1 -19.08,-19.08 Z" fill-rule="evenodd"/></svg></div><div id="spr11_4761a5f4" style="left:15px;top:50px;"><img id="img2_4761a5f4" src="data/img29.png" width="189" height="281" alt="\
+ 3, 4, 5 disebut Tripel Pythagoras, karena\
+\
+\
+\
+\
+\
+\
+\
+b.  5, 12, 13 disebut Tripel Pythagoras, karena\
+\
+\
+\
+\
+\
+\
+\
+Coba Teman – teman perhatikan  Soal berikut!\
+1. Berikut ini manakah yang merupakan panjang sisi segitiga siku-siku\
+a. 4 𝑐𝑚, 5 𝑐𝑚, dan 6 𝑐𝑚\
+b. 4 𝑐𝑚, 6 𝑐𝑚, dan 8 𝑐𝑚\
+c. 6 𝑐𝑚, 8 𝑐𝑚, dan 10 𝑐𝑚\
+\
+Yang harus teman – teman ingat !!!\
+Dikatakan segitiga siku – siku jika kuadrat sisi terpanjang sama dengan jumlah kuadrat sisi lainya    𝒄 𝟐  = 𝒂 𝟐  +  𝒃 𝟐  .\
+Dikatakan  segitiga lancip jika kuadrat sisi terpanjang lebih kecil dari jumlah kuadrat sisi lainya           𝒄 𝟐     𝒂 𝟐  +  𝒃 𝟐  \
+Dikatakan segitiga tumpul jika kuadrat sisi terpanjang lebih besar dari jumlah kuadrat sisi lainya         𝒄 𝟐    𝒂 𝟐  +  𝒃 𝟐  \
+\
+Disini kita akan mencoba untuk mengerjakan contoh soal di atas.\
+"/></div><div id="spr12_4761a5f4" style="left:171.534px;top:2.887px;"><img id="img3_4761a5f4" src="data/img15.png" width="36" height="36" alt="" style="left:0.179px;top:-0.347px;"/></div><div id="spr13_4761a5f4" style="left:38px;top:60px;"><img id="img4_4761a5f4" src="data/img30.png" width="159" height="49" alt="PENYELESAIAN\
+ 3 2  +  4 2  = 9 + 16\
+                = 25\
+                =  5 2  \
+3 cm, 4 cm, dan 5 cm merupakan panjang sisi-sisi segitiga siku-siku. "/></div><div id="spr14_4761a5f4" style="left:37px;top:136px;"><img id="img5_4761a5f4" src="data/img31.png" width="149" height="49" alt="PENYELESAIAN\
+ 5 2  +  12 2  = 25 + 144\
+                = 169\
+                =  13 2   \
+5 cm, 12 cm, dan 13 cm merupakan panjang sisi-sisi segitiga siku-siku."/></div></div></div>', '{"s":[]}');})();
